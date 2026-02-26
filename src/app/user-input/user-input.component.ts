40b28b67368d8b9d5eea5164ca4537fa36288b87
Lena Herrmann <herrmann-lena@t-online.de>
2026-02-26 10:24:32 +0100

    Extracting Form Values with 2-Way-Binding

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  enteredMortgageAmount = '0';
+  enteredMortgageTerm = '0'; 
+  enteredInterestRate = '0'; 
+  enteredMortgageType = ''; 
 
+  onSubmit() {
+    console.log(this.enteredMortgageAmount);
+    console.log(this.enteredMortgageTerm);
+    console.log(this.enteredInterestRate);
+    console.log(this.enteredMortgageType);
+
+    this.enteredMortgageAmount = '';
+    this.enteredMortgageTerm = ''; 
+    this.enteredInterestRate = ''; 
+    this.enteredMortgageType = '';
+  }
 }
