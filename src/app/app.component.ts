5de0fa867e6650f4bde9d74925202bc0174e2865
Lena Herrmann <herrmann-lena@t-online.de>
2026-02-26 08:40:53 +0100

    Create Components and setup Layout basically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
+import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
+import { ResultsComponent } from './results/results.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, UserInputComponent, ResultsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
